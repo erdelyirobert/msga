b86d10ee48d3fe21c41ca059540bbb9441802f27
Robi <divpp3@inf.elte.hu>
2021-03-14 17:48:29 +0100

    robi

--- src/Main.java
+++ src/Main.java
@@ -3,7 +3,7 @@ import gui.MainMenu;
 public class Main {
     public static void main(String[] args) {
         System.out.println("aaasd");
-
+        System.out.println("intellij");
         MainMenu mainMenu = new MainMenu();
 
 

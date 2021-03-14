c53d7a85e861dd1bd335d4704a68a29f96382af4
David <ferenczi.david55@gmail.com>
2021-03-14 17:33:33 +0100

    mukodik

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,7 @@ import gui.MainMenu;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("aa");
+        System.out.println("aaasd");
 
         MainMenu mainMenu = new MainMenu();
 

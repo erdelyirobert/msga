9074082be77f547901cdda76d5a46e68c786cc28
thebird97 <balint.mb@gmail.com>
2021-03-15 19:01:33 +0100

    Már működik az exit

--- src/gui/ThemeParkGUI.java
+++ src/gui/ThemeParkGUI.java
@@ -4,11 +4,17 @@ import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.WindowEvent;
 
 public class ThemeParkGUI {
     JFrame frame = new JFrame("Theme park");
 
-    public ThemeParkGUI(String title){
+    /**
+     * Instantiates a new Theme park gui.
+     *
+     * @param title the title
+     */
+    public ThemeParkGUI(String title) {
         frame.setSize(500, 500);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setResizable(true);
@@ -23,23 +29,39 @@ public class ThemeParkGUI {
         JMenuBar menuBar = new JMenuBar(); // creation of menubar
         frame.setJMenuBar(menuBar); //adding the menubar to the frame
 
-        JMenu gameMenu = new JMenu("Game"); //creation of the "Game" dropdown on the menubar
-        menuBar.add(gameMenu); //adding it to the menubar
+        JMenuBar menub = new JMenuBar();
+        JMenu settingsMenu = new JMenu("Settings");
+        JMenuItem saveMenuItem = new JMenuItem("Save");
+        JMenuItem exitMenuItem = new JMenuItem("Exit");
 
-        JMenu saveMenuItem = new JMenu("Save"); //save option in the dropdown beneath the "Game"
-        gameMenu.add(saveMenuItem); //adding it beneath the "Game" to the gamemenu
+        menub.add(settingsMenu);
+        menub.add(exitMenuItem);
+        settingsMenu.add(saveMenuItem);
+        settingsMenu.add(exitMenuItem);
 
-        JMenu exitMenuItem = new JMenu("Exit"); //exit option in the dropdown beneath the "Game"
-        gameMenu.add(exitMenuItem); //adding it beneath the "Game" to the gamemenu
+        this.frame.setJMenuBar(menub);
+
+        /*
+         * ActionListeners
+         * Upon clicking on the "Save" option the game will save
+         */
+        saveMenuItem.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent ae) {
+                //todo
+                throw new UnsupportedOperationException("Not defined");
+            }
+        });
 
         /**
-         * ActionListener
+         * ActionListeners
+         *
          * Upon clicking on the "Exit" option the game will stop running and will close
          */
         exitMenuItem.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent ae) {
-                System.exit(0);
+                frame.dispatchEvent(new WindowEvent(frame, WindowEvent.WINDOW_CLOSING));
             }
         });
 

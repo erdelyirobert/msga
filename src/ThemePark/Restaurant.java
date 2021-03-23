87a635b5aabac563d1147a93a42dd60964c68a27
thebird97 <balint.mb@gmail.com>
2021-03-23 20:00:37 +0100

    added Restaurant.java

---
+++ src/ThemePark/Restaurant.java
@@ -0,0 +1,11 @@
+package ThemePark;
+
+public class Restaurant extends Building {
+    public Restaurant(boolean inConstruction, int constructionTime, int buildPrice, int location_X, int location_Y, int range) {
+        super(inConstruction, constructionTime, buildPrice, location_X, location_Y, range);
+    }
+
+    public void startTrashTimer() {
+        //TODO
+    }
+}

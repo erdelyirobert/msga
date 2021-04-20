45ff2368e133e83e4bff17ac6cb62ad852cfe078
divpp3 <divpp3@inf.elte.hu>
2021-04-20 22:07:31 +0200

    reduceConstructionTime() cleanTrash() leaveTrash() addGameToGuest() playGuest()

--- src/ThemePark/Guest.java
+++ src/ThemePark/Guest.java
@@ -1,17 +1,90 @@
 package ThemePark;
 
-import java.awt.event.ActionEvent;
+import board.TPBoard;
+
 import java.util.ArrayList;
 import java.util.Random;
-import javax.swing.Timer;
 
 public class Guest extends Person{
     Random random = new Random();
     int r = random.nextInt(4);
     private int mood = 6;
     public int direction = r;           //actual direction
-    private boolean leaveTrash = false;
-    public Timer trashTimer;
+    private boolean activateTimer = false;
+    private int trashTimer = 50;
+    private String targetGame = "";
+    private boolean inGame = false;
+    private int playingTimer = 100;
+
+
+
+
+    public boolean isInGame(){
+        if(inGame){
+            playingTimer--;
+            if(playingTimer==0){
+                playingTimer = 100;
+                inGame = false;
+                targetGame="";
+                return false;
+            } else {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public void setInGame(boolean inGame) {
+        this.inGame = inGame;
+    }
+
+
+
+    public boolean canLeaveTrash(){              // ha aktiv a timer, akkor csökkenti a trashTimert, ha pedig 0, akkor leteheti a kukát
+        if(activateTimer){
+            trashTimer--;
+            if(trashTimer==0){
+                trashTimer = 50;
+                activateTimer = false;
+                return true;
+            } else {
+                return false;
+            }
+        }
+        return false;
+    }
+
+    public boolean getInGame() {
+        return inGame;
+    }
+
+    public int getPlayingTimer() {
+        return playingTimer;
+    }
+
+    public void setPlayingTimer(int playingTimer) {
+        this.playingTimer = playingTimer;
+    }
+    public String getTargetGame() {
+        return targetGame;
+    }
+
+    public void setTargetGame(String targetGame) {
+        this.targetGame = targetGame;
+    }
+    public boolean getActivateTimer() {
+        return activateTimer;
+    }
+    public int getTrashTimer() {
+        return trashTimer;
+    }
+
+    public void setTrashTimer(int trashTimer) {
+        this.trashTimer = trashTimer;
+    }
+    public void setActivateTimer(boolean activateTimer) {
+        this.activateTimer = activateTimer;
+    }
 
 
 
@@ -37,18 +110,5 @@ public class Guest extends Person{
         this.direction = direction;
     }
 
-    public boolean getLeaveTrash() {
-        return leaveTrash;
-    }
 
-    public void setLeaveTrash(boolean leaveTrash) {
-        this.leaveTrash = leaveTrash;
-    }
-
-    public void startTrashTimer(){
-        trashTimer = new Timer(5000, (ActionEvent e) -> {
-            leaveTrash = true;
-        });
-        trashTimer.start();
-    }
 }

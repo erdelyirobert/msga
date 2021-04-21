f6e9318e9f5d4789bbd7298c55d9e4445217e971
Madár Bálint <mrpblg@inf.elte.hu>
2021-04-21 18:20:52 +0200

    Distance test added

--- src/test/java/board/TPBoardTest.java
+++ src/test/java/board/TPBoardTest.java
@@ -1,33 +1,30 @@
 package board;
 
-import org.junit.Assert;
-import org.junit.Test;
-
-
 import ThemePark.Building;
 import ThemePark.Guest;
 import ThemePark.Trash;
 import ThemePark.Worker;
+import org.junit.Assert;
+import org.junit.Test;
 
-
+import java.io.IOException;
 import java.util.ArrayList;
 
 
 public class TPBoardTest {
+    int x = 0;
+    int y = 0;
     //JUnit Suite Test
     private int segmentSize = 20; //size of one grid
-    int x=0;
-    int y=0;
-
 
     @Test
-    public void ArrayListTest(){
-         ArrayList<Building> buildings = new ArrayList<Building>();
-         ArrayList<Guest> guests = new ArrayList<Guest>();
-         ArrayList<Worker> workers = new ArrayList<Worker>();
-         ArrayList<Connection> connections = new ArrayList<Connection>();
-         ArrayList<Building> roads = new ArrayList<Building>();
-         ArrayList<Trash> trashes = new ArrayList<Trash>();
+    public void ArrayListTest() {
+        ArrayList<Building> buildings = new ArrayList<Building>();
+        ArrayList<Guest> guests = new ArrayList<Guest>();
+        ArrayList<Worker> workers = new ArrayList<Worker>();
+        ArrayList<Connection> connections = new ArrayList<Connection>();
+        ArrayList<Building> roads = new ArrayList<Building>();
+        ArrayList<Trash> trashes = new ArrayList<Trash>();
 
 
         Building starterRoad = new Building("ROAD", 0, 0, 60, 80, segmentSize, segmentSize);
@@ -41,15 +38,26 @@ public class TPBoardTest {
         buildings.add(new Building("TREE", 0.0, 10, x - (x % segmentSize), y - (y % segmentSize), segmentSize * 2, segmentSize * 2));
 
         Assert.assertNotNull(buildings);
-        Assert.assertEquals("Not good vale for segment",segmentSize,20);
+        Assert.assertEquals("Not good vale for segment", segmentSize, 20);
 
 
     }
 
 
     @Test
-    public void MoneyTest(){
+    public void MoneyTest() {
 
+
+    }
+
+    @Test
+    public void CoordianteTests() throws IOException {
+        TPBoard board = new TPBoard();
+        double distance = board.coordinateDistance(0, 4, 1, 4);
+        Assert.assertEquals("Distance error1", (double) 5.0, distance, 0);
+
+        double distance2 = board.coordinateDistance(1, 4, 0, 4);
+        Assert.assertEquals("Distance error2", (double) 5.0, distance2, 0);
 
     }
 
